0e9104fba2fa0f81bc00aa678c91c86db2376e69
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2023-01-25 15:35:15 +0100

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="text-red-400">
+        Click on the Vite and React logoos to learn moreeee
+      </p>
+    </div>
+  )
+}
+
+export default App
