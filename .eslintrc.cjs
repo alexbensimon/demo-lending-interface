28e644406799503c13802f8f8c23ef225da3f526
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2023-01-26 15:54:58 +0100

    Add ESLint & Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "eslint-config-prettier", // Keep at the end
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/button-has-type": "warn",
+    "no-console": "warn",
+  },
+};
